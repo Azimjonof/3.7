b6d84f4ea7ed62e640e27b84ff12e7fa622b54ea
Ramazon azimjonov <ramazonazimjonov@Ramazons-MacBook-Pro.local>
2025-02-26 03:06:03 +0500

    3.7

---
+++ Desktop/Najot talim/js kirish /3.7 lesson/homework/src/main.js
@@ -0,0 +1,67 @@
+// 1-masala
+// let a = 6
+// let p = a * 4
+// console.log(p);
+
+// 2-masala
+// let a = 6
+// let s = a * a 
+// console.log(s);
+
+// 3-masala
+// let a = 6
+// let b = 4
+// let s = a * b 
+// let p = 2 * (a + b)
+// console.log(s);
+// console.log(p);
+
+// 4-masala
+// let d = 6
+// let l = d * Math.PI
+// console.log(l);
+
+// 5-masala
+// let a = 6
+// let v = Math.pow(a,3)
+// let s = 6 * Math.pow(a,2)
+// console.log(v);
+// console.log(s);
+
+// 6-masala
+// let a = 6
+// let b = 6
+// let c = 6
+// let v = a * b * c 
+// let s = 2 * (a * b + b * c + a * c)
+// console.log(v);
+// console.log(s);
+
+// 7-masala
+// let r = 6
+// let l = 2 * Math.PI * r 
+// let s = Math.PI * Math.pow(r,2)
+// console.log(l);
+// console.log(s);
+
+// 8-masala
+// let a = 6
+// let b = 6 
+// let s = (a + b)/2
+// console.log(s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
